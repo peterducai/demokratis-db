c43416ff08b54341b19fa2e567240c71b55734a1
peter ducai <peter.ducai@gmail.com>
2018-11-01 22:51:46 +0100

    wait for db

--- docker_rebuild.sh
+++ docker_rebuild.sh
@@ -9,4 +9,5 @@ sudo docker build . -t "peterducai/demokratis-db:v0.0.1" -f Dockerfile_fakedata
 sudo docker tag peterducai/demokratis-db:v0.0.1  peterducai/demokratis-db:latest
 sudo docker run -d --name demokratis-db1 -p 9999:5432 peterducai/demokratis-db:latest  
 sudo docker ps -a
+sleep 5
 sudo docker logs demokratis-db1
