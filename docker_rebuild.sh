0dd066727aad47b0202595a9b1c35bb65f26c2a1
daemonna <32168685+peterducai@users.noreply.github.com>
2018-11-06 15:17:08 +0100

    Update docker_rebuild.sh

--- docker_rebuild.sh
+++ docker_rebuild.sh
@@ -3,8 +3,8 @@
 sudo docker stop demokratis-db1  
 sudo docker rm demokratis-db1   
 sudo docker rmi peterducai/demokratis-db:v0.0.1
-sudo docker rm `docker ps --no-trunc -aq` 
-sudo docker images -q --filter "dangling=true" | xargs docker rmi  
+sudo docker rm `sudo docker ps --no-trunc -aq` 
+sudo docker images -q --filter "dangling=true" | xargs sudo docker rmi  
 sudo docker build . -t "peterducai/demokratis-db:v0.0.1" -f Dockerfile_fakedata 
 sudo docker tag peterducai/demokratis-db:v0.0.1  peterducai/demokratis-db:latest
 sudo docker run -d --name demokratis-db1 -p 9999:5432 peterducai/demokratis-db:latest  
